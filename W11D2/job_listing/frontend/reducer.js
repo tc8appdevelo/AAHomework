c5502ef2464cccc53971f164d5f03a2aca82aab2
tc8appdevelo <tc8appdevelo@gmail.com>
2021-12-13 23:34:06 -0600

    finished Job Listing hw project

---
+++ W11D2/job_listing/frontend/reducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+  city: "Please Select", 
+  jobs: []
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "SWITCH_LOCATION":
+      return { jobs: action.jobs, city: action.city }
+    default:
+      return state;
+  }
+};
+
+
+export default reducer;
